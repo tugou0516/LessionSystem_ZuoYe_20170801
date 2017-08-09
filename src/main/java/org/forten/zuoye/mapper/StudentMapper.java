f24260504c78ae174c6ec808a8aac023a99a3e66
tugou0516 <hihi05160696@126.com>
2017-08-09 10:33:03 +0800

    删除李贺的测试类引用

--- src/main/java/org/forten/zuoye/mapper/StudentMapper.java
+++ src/main/java/org/forten/zuoye/mapper/StudentMapper.java
@@ -1,8 +1,6 @@
 package org.forten.zuoye.mapper;
 
 import org.apache.ibatis.annotations.*;
-import org.forten.zuoye.dto.SelectByIdHelper;
-import org.forten.zuoye.dto.SelectByIdQo;
 import org.forten.zuoye.dto.StudentDto4Mybatis;
 import org.forten.zuoye.model.Student;
 
@@ -23,8 +21,6 @@ public interface StudentMapper {
     @Results(id="studentMap",value={
             @Result(property="loginName",column="login_name")
     })
-    @SelectProvider(type = SelectByIdHelper.class,method = "getSql")
-    Student selectByIdM(SelectByIdQo qo);
 
     @ResultMap("studentMap")
     @Select("SELECT login_name,password,name,gender,position,birthday,tel,email,score FROM test_student  ")
