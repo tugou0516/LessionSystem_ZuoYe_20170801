c7539eb531310eb4524d2bf6083e6126348ee5f8
Your Name <you@example.com>
2017-08-10 16:44:04 +0800

    2017-08-10 Course4StuShowRo更新

--- src/main/java/org/forten/zuoye/dto/course/Course4StuShowRo.java
+++ src/main/java/org/forten/zuoye/dto/course/Course4StuShowRo.java
@@ -1,11 +1,17 @@
 package org.forten.zuoye.dto.course;
 
+import org.forten.utils.common.DateUtil;
+
 import java.util.Date;
+
+import static org.forten.utils.common.DateUtil.DATETIME_PATTERN;
+import static org.forten.utils.common.DateUtil.currentTime;
 
 /**
  * Created by Administrator on 2017/8/9.
  */
 public class Course4StuShowRo {
+    private int courseId;
     private String courseName;
     private String teacher;
     private String classRoom;
@@ -21,7 +27,8 @@ public class Course4StuShowRo {
     public Course4StuShowRo() {
     }
 
-    public Course4StuShowRo(String courseName, String teacher, String classRoom, Date courseEndTime, Date classStartTime, Date classEndTime, int score, int courseStatus, int attendStatus, Date chooseTime, Date updateTime) {
+    public Course4StuShowRo(int courseId, String courseName, String teacher, String classRoom, Date courseEndTime, Date classStartTime, Date classEndTime, int score, int courseStatus, int attendStatus, Date chooseTime, Date updateTime) {
+        this.courseId = courseId;
         this.courseName = courseName;
         this.teacher = teacher;
         this.classRoom = classRoom;
@@ -33,6 +40,56 @@ public class Course4StuShowRo {
         this.attendStatus = attendStatus;
         this.chooseTime = chooseTime;
         this.updateTime = updateTime;
+    }
+
+    public String getClassStartTimeStr(){
+        return classStartTime == null ? "" : DateUtil.convertDateToString(classStartTime, DATETIME_PATTERN);
+    }
+    public String getClassEndTimeStr(){
+        return classEndTime == null ? "" : DateUtil.convertDateToString(classEndTime, DATETIME_PATTERN);
+    }
+    public String getCourseEndTimeStr(){
+        return courseEndTime == null ? "" : DateUtil.convertDateToString(courseEndTime, DATETIME_PATTERN);
+    }
+
+    public boolean getChooseAble(){
+        if((courseEndTime.getTime()-currentTime().getTime())>0)
+            return true;
+        else
+            return false;
+    }
+
+    public String getAttendStatusStr(){
+        if(attendStatus==1)
+            return "出勤";
+        else if(attendStatus==2)
+            return "旷课";
+        else if(attendStatus==3)
+            return "请假";
+        else
+            return "未知";
+    }
+    public String getCourseStatusStr(){
+        if(courseStatus==1)
+            return "已选";
+        else if(courseStatus==2)
+            return "排队中";
+        else if(courseStatus==4)
+            return "已修";
+        else
+            return "未知";
+    }
+
+    public long getClassLength(){
+        return (classEndTime.getTime()-classStartTime.getTime())/1000/60;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
     }
 
     public String getCourseName() {
@@ -126,15 +183,16 @@ public class Course4StuShowRo {
     @Override
     public String toString() {
         return "Course4StuShowRo{" +
-                "courseName='" + courseName + '\'' +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
                 ", teacher='" + teacher + '\'' +
                 ", classRoom='" + classRoom + '\'' +
                 ", courseEndTime=" + courseEndTime +
                 ", classStartTime=" + classStartTime +
                 ", classEndTime=" + classEndTime +
                 ", score=" + score +
-                ", courseStatus='" + courseStatus + '\'' +
-                ", attendStatus='" + attendStatus + '\'' +
+                ", courseStatus=" + courseStatus +
+                ", attendStatus=" + attendStatus +
                 ", chooseTime=" + chooseTime +
                 ", updateTime=" + updateTime +
                 '}';
