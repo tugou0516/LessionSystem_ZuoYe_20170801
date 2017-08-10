1cadf1136d6f781434efd323c7c26279d4d730f6
Your Name <you@example.com>
2017-08-10 16:38:55 +0800

    2017-08-10StudnetAction更新

--- src/main/java/org/forten/zuoye/action/StudentAction.java
+++ src/main/java/org/forten/zuoye/action/StudentAction.java
@@ -2,6 +2,7 @@ package org.forten.zuoye.action;
 
 import org.forten.zuoye.bo.StudentBo;
 import org.forten.zuoye.dto.common.Message;
+import org.forten.zuoye.dto.course.Course4StuShowRo;
 import org.forten.zuoye.dto.student.Student4SaveDto;
 import org.forten.zuoye.dto.student.Student4ShowRo;
 import org.springframework.stereotype.Controller;
@@ -10,22 +11,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/8.
  */
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/student/")
 public class StudentAction {
     @Resource
     private StudentBo bo;
 
-    @RequestMapping("show")
+    @RequestMapping("showPrivateInfo")
     public @ResponseBody
     Student4ShowRo getOne(){
-//        从session获得学员id
+        //      listOtherCourse(@RequestBody int id)
+        //        从session获得学员id
         int id=1;
         return bo.doSelectByIdH(id);
+    }
+
+    @RequestMapping("showCompletedCourse")
+    public @ResponseBody
+    List<Course4StuShowRo> listCompletedCourse(){
+        //      listOtherCourse(@RequestBody int id)
+        //        从session获得学员id
+        int id=1;
+        return bo.listCompletedCourse(id);
+    }
+
+    @RequestMapping("showOtherCourse")
+    public @ResponseBody
+    List<Course4StuShowRo> listOtherCourse(){
+        //      listOtherCourse(@RequestBody int id)
+        //      从session获得学员id
+        int id=1;
+        return bo.listOtherCourse(id);
     }
 
 }
