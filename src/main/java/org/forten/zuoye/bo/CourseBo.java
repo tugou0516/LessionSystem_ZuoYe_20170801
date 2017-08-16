253bbb3453a17144f61de84cfaa7581c15f5d9be
ZeKe66 <1345392684@qq.com>
2017-08-16 12:03:02 +0800

    sql文件

--- src/main/java/org/forten/zuoye/bo/CourseBo.java
+++ src/main/java/org/forten/zuoye/bo/CourseBo.java
@@ -1,10 +1,10 @@
 package org.forten.zuoye.bo;
 
-import org.forten.utils.system.BeanPropertyUtil;
 import org.forten.zuoye.dao.HibernateDao;
-import org.forten.zuoye.dto.Course4ShowDto;
+import org.forten.zuoye.dto.course.Course4ShowDto;
+import org.forten.zuoye.dto.course.CourseId;
 import org.forten.zuoye.model.Course;
-import org.forten.zuoye.model.Student;
+import org.forten.zuoye.model.LinedCS;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,17 +17,36 @@ import java.util.Map;
 /**
  * Created by student3 on 2017/8/9.
  */
-@Service("course")
+@Service("courseBo")
 public class CourseBo {
 
     @Resource
     private HibernateDao dao;
 
     @Transactional(readOnly = true)
-    public List<Course4ShowDto> doListAll() {
-        String hql = "SELECT new org.forten.zuoye.dto.Course4ShowDto(id, name, classRoom, teacher, courseStartTime, courseEndTime, classStartTime, classEndTime, score, classCapacity) FROM Course";
+    public List<Course4ShowDto> doListAll(int id) {
+        String hql01 = "SELECT new org.forten.zuoye.dto.course.CourseId(courseId,chooseStatus) FROM LinedCS WHERE studentId= :id ";
         Map<String, Object> params = new HashMap<>();
-        List<Course4ShowDto> list = dao.findBy(hql, params);
-        return list;
+        System.out.println(id);
+        params.put("id", id);
+        List<CourseId> selectedCourse = dao.findBy(hql01, params);
+
+        String hql02 = "SELECT new org.forten.zuoye.dto.course.Course4ShowDto(id, name, classRoom, teacher, courseStartTime, courseEndTime, classStartTime, classEndTime, score, classCapacity) " +
+                "FROM Course ";
+        List<Course4ShowDto> allCourse = dao.findBy(hql02);
+
+        for (Course4ShowDto x : allCourse) {
+            for (CourseId i : selectedCourse) {
+                if (i.getCourseId() == x.getCourseId()) {
+                    x.setChooseStatus(i.getChooseStatus());
+                    if(i.getChooseStatus()==1) {
+                        x.setChoose(false);
+                    }else {
+                        x.setChoose(true);
+                    }
+                }
+            }
+        }
+        return allCourse;
     }
 }
