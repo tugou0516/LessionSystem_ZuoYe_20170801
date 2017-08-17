338bbc8e2d761e4ddeeca3248379a922deee51ef
Your Name <you@example.com>
2017-08-17 16:11:09 +0800

    20170818计较Bo更新

--- src/main/java/org/forten/zuoye/bo/CoManageBo.java
+++ src/main/java/org/forten/zuoye/bo/CoManageBo.java
@@ -1,11 +1,19 @@
 package org.forten.zuoye.bo;
 
 import org.forten.utils.common.StringUtil;
+import org.forten.utils.system.BeanPropertyUtil;
 import org.forten.utils.system.PageInfo;
 import org.forten.zuoye.dao.HibernateDao;
+import org.forten.zuoye.dto.common.Message;
 import org.forten.zuoye.dto.common.RoWithPage;
 import org.forten.zuoye.dto.course.Course4CoManageRo;
 import org.forten.zuoye.dto.course.CourseQo;
+import org.forten.zuoye.dto.course.Dto4SaveCourse;
+import org.forten.zuoye.dto.course.Dto4UpdateCourse;
+import org.forten.zuoye.dto.student.CourseStudentQo;
+import org.forten.zuoye.dto.student.Student4ShowRo;
+import org.forten.zuoye.model.Course;
+import org.forten.zuoye.model.Student;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -81,4 +89,76 @@ public class CoManageBo {
 
         return new RoWithPage<>(list,page);
     }
+
+    //添加课程
+    @Transactional
+    public Message doSaveCourse(Dto4SaveCourse cou){
+        Course course = new Course();
+        BeanPropertyUtil.copy(course,cou);
+        try {
+            hDao.save(course);
+            return new Message("课程添加成功");
+        }
+        catch (Exception e){
+            return new Message("课程添加失败");
+        }
+    }
+
+    //删除课程
+    @Transactional
+    public Message doDeleteCourse(int id){
+        try{
+            hDao.delete(id,Course.class);
+            return new Message("课程删除成功");
+        }
+        catch (Exception e){
+            return new Message("课程删除失败");
+        }
+    }
+
+    //更新课程信息
+    @Transactional
+    public Message doUpdateCourse(int id, Dto4UpdateCourse cou) {
+        Course course = hDao.getById(id, Course.class);
+        BeanPropertyUtil.copy(course, cou);
+        try {
+            hDao.update(course);
+            return new Message("课程更新成功");
+        } catch (Exception e) {
+            return new Message("课程更新失败");
+        }
+    }
+
+    //分页查询课程学生 可按登录名和姓名模糊查询
+    @Transactional
+    public RoWithPage<Student4ShowRo> doListStudentByCourse(CourseStudentQo qo, int id){
+        String countHql="SELECT count(s.id) " +
+                "FROM Student s RIGHT JOIN LinedCS l ON (s.id=l.studentId) WHERE l.courseId=:id ";
+        String hql="SELECT new org.forten.zuoye.dto.student.Student4ShowRo(s.id, s.loginName, s.name, s.gender, s.position, s.birthday, s.tel, s.email) " +
+                "FROM Student s RIGHT JOIN LinedCS l ON (s.id=l.studentId) WHERE l.courseId=:id ";
+        Map<String,Object> param = new HashMap<>();
+        param.put("id",id);
+
+        if(StringUtil.hasText(qo.getLoginName())){
+            countHql = countHql + "AND s.loginName LIKE :loginName ";
+            hql = hql + "AND s.loginName LIKE :loginName ";
+            param.put("loginName","%"+qo.getLoginName()+"%");
+        }
+
+        if(StringUtil.hasText(qo.getName())){
+            countHql = countHql + "AND name LIKE :name ";
+            hql = hql + "AND name LIKE :name ";
+            param.put("name","%"+qo.getName()+"%");
+        }
+
+        long count=hDao.findOneBy(countHql,param);
+        if (count==0)
+            return RoWithPage.EMPTY_RO;
+        else{
+            PageInfo page =PageInfo.getInstance(qo.getPageNo(),qo.getPageSize(),count);
+            List<Student4ShowRo> studentList =hDao.findBy(hql,param,(int)page.getFirstResultNum(),page.getPageSize());
+            return new RoWithPage<>(studentList,page);
+        }
+    }
+
 }
