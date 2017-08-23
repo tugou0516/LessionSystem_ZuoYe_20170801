ce3bd9435b02f442aa14fa031defede1a4692c09
towins <958370187@qq.com>
2017-08-23 15:24:40 +0800

    文件上传

--- src/main/java/org/forten/zuoye/filter/Auth1Filter.java
+++ src/main/java/org/forten/zuoye/filter/Auth1Filter.java
@@ -8,11 +8,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.forten.zuoye.dto.student.LoginStudent;
+import org.forten.zuoye.dto.student.LoginedStudent;
+import org.forten.zuoye.dto.student.RenShiAdmin;
 
 /**
  * 认证过滤器
@@ -27,11 +27,25 @@ public class Auth1Filter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		HttpSession session = req.getSession();
-		LoginStudent user = (LoginStudent) session.getAttribute("loginStudent");
+		LoginedStudent user = (LoginedStudent) session.getAttribute("logined");
+		RenShiAdmin user02 = (RenShiAdmin) session.getAttribute("renShiAdmin");
+		RenShiAdmin user03 = (RenShiAdmin) session.getAttribute("jiJiaoAdmin");
 		if (user == null) {
 			res.sendRedirect("/student/login.html");
 		} else {
-			req.setAttribute("loginStudent", user);
+			req.setAttribute("logined", user);
+			chain.doFilter(req, response);
+		}
+		if (user02 == null) {
+			res.sendRedirect("/student/login.html");
+		} else {
+			req.setAttribute("logined", user02);
+			chain.doFilter(req, response);
+		}
+		if (user03 == null) {
+			res.sendRedirect("/student/login.html");
+		} else {
+			req.setAttribute("logined", user03);
 			chain.doFilter(req, response);
 		}
 	}
